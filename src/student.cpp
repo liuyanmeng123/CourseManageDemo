a448e0f4fa4071d1a29569364ef184f31d68c926
liuyanmeng123 <liuym0518@sina.com>
2021-08-31 16:14:26 +0800

    finish student class

--- src/student.cpp
+++ src/student.cpp
@@ -4,33 +4,36 @@
 
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 #include "student.h"
 #include "global.h"
+#include "utils.h"
 
 void Student::selectCourse(){		
 	string coursename;
-	//string coursetype;
-	//unsigned int credit;
-	//int time;
-	//string teachername;
 
-	cout << "请输入想要选择的课程名称：" << endl;
+	showInfo("请输入想要选择的课程名称：");
 	cin >> coursename;
+
 	Context& context = Context::get_instance();
 	string course_id = context.getCourseID(coursename);
+
 	if (!course_id.empty()) {
+	    Course* course_p = context.getCourse(course_id,coursename);
 	    course_id_list.push_back(course_id);
-	    cout << "选课成功！" << endl;
+	    course_p->stu_id_list.push_back(this->getId());
+
+	    showInfo("选课成功！");
 	} else {
-	    cout << "该课程未开设！" << endl;
+	    showInfo("该课程未开设！");
 	}
 } 
 
 void Student::dropCourse(){
 	string coursename;
 	int i;
-	cout << "请输入想退选的课程id：" << endl;
+	showInfo("请输入想要退选的课程名称：");
 	cin >> coursename;
 
 	Context& context = Context::get_instance();
@@ -39,21 +42,24 @@ void Student::dropCourse(){
 	if (!course_id.empty()) {
 	    for(i = 0;i < course_id_list.size(); i++ ){
 	        if(course_id_list[i] == course_id){
+	            Course* course_p = context.getCourse(course_id,coursename);
+
 	            course_id_list.erase(course_id_list.begin() + i);
-	            cout << "退选成功！" << endl;
+	            course_p->stu_id_list.erase(remove(course_p->stu_id_list.begin(),course_p->stu_id_list.end(),this->getId()),course_p->stu_id_list.end());
+
+	            showInfo("退课成功！");
 	            break;
 	        }
 	    }
-	    cout << "退选成功！" << endl;
 	} else {
-	    cout << "该课程未开设！" << endl;
+	    showInfo("该课程未开设！");
 	}
 } 
 
 void Student::getCourseInfo(){
     string coursename;
     int i;
-    cout << "请输入想要查询的课程名称：" << endl;
+    showInfo("请输入想要查询的课程名称：");
     cin >> coursename;
 
     Context& context = Context::get_instance();
@@ -61,17 +67,16 @@ void Student::getCourseInfo(){
     if (!course_id.empty()) {
         for(i = 0;i < course_id_list.size(); i++ ){
             if(course_id_list[i] == course_id){
-                Course _course = context.getCourse(course_id,coursename);
-                cout << "课程名称：" << _course.getName() << endl;
-                cout << "课程类型：" << _course.getCourseType() << endl;
-                cout << "课程学分：" << _course.getCredit() << endl;
-                cout << "课程时长：" << _course.getTime() << endl;
-                cout << "课程教师：" << _course.getTeacherName() << endl;
+                Course* course_ptr = context.getCourse(course_id,coursename);
+                cout << "课程名称：" << course_ptr->getName() << endl;
+                cout << "课程类型：" << course_ptr->getCourseType() << endl;
+                cout << "课程学分：" << course_ptr->getCredit() << endl;
+                cout << "课程时长：" << course_ptr->getTime() << endl;
+                cout << "课程教师：" << course_ptr->getTeacherName() << endl;
                 }
                 break;
-            }
         }
-
+    }
 }
 
 Student::Student() {
